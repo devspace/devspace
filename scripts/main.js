f6e926dc6f1fa4b85d97702a473667ad9a948e75
Zeno Rocha <hi@zenorocha.com>
2015-11-13 09:28:27 -0800

    Adds not found route

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Link } from 'react-router'
+import { Router, Route, Link } from 'react-router';
 
 import App from './app';
 import Home from './home';
+import NotFound from './notFound';
 
 var routes = (
 	<Router>
 		<Route path="/" component={Home}/>
 		<Route path="/app" component={App}/>
+		<Route path="*" component={NotFound}/>
 	</Router>
 )
 
