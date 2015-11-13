3a92d3825213a9080b1d264153e0a33d5a7e4f37
Zeno Rocha <hi@zenorocha.com>
2015-11-13 02:54:17 -0800

    Moves navigation to a separated component

---
+++ scripts/nav.js
@@ -0,0 +1,26 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Nav = React.createClass({
+	render: function() {
+		return (
+			<div className="nav">
+				<ul>
+					<li><a className="nav-selected" href="/" title="Home"><span className="octicon octicon-home"></span></a></li>
+					<li><a href="#" title="Add column"><span className="octicon octicon-plus"></span></a></li>
+					<li><a href="#" title="Settings"><span className="octicon octicon-gear"></span></a></li>
+				</ul>
+				<div className="footer">
+					<ul>
+						<li><a href="#" title="Announcements"><span className="octicon octicon-megaphone"></span></a></li>
+						<li><a href="mailto:support@devspace.io" title="Ask a question"><span className="octicon octicon-question"></span></a></li>
+						<li><a href="https://github.com/devspace/bugs" title="Report a bug"><span className="octicon octicon-bug"></span></a></li>
+						<li><a href="#" title="Sign Out"><span className="octicon octicon-sign-out"></span></a></li>
+					</ul>
+				</div>
+			</div>
+		)
+	}
+});
+
+ReactDOM.render(<Nav/>, document.querySelector('.container'));
