3730c7b0e87b07a3d30559a075c6a8ae53d4a137
Zeno Rocha <hi@zenorocha.com>
2015-11-13 19:22:43 -0800

    Improves home component styling and markup

--- scripts/home.js
+++ scripts/home.js
@@ -3,14 +3,15 @@ import React from 'react';
 class Home extends React.Component {
 	render() {
 		return (
-			<div id="container">
-				<div id="particles"></div>
-				<img src="./images/icon.png" height="256" width="256" alt="Logo" />
-				<h1>DevSpace</h1>
-				<h2>The best way to stay in touch with what's happening now on GitHub</h2>
-				<a className="btn" href="app.html">
-					<span className="octicon octicon-mark-github"></span> Login with GitHub
-				</a>
+			<div className="home">
+				<main className="home-container">
+					<img className="home-logo" src="./images/icon.png" height="256" width="256" alt="DevSpace Logo" />
+					<h1 className="home-title">DevSpace</h1>
+					<h2 className="home-subtitle">The best way to stay in touch with what's happening now on GitHub</h2>
+					<button className="home-btn">
+						<span className="octicon octicon-mark-github"></span> Login with GitHub
+					</button>
+				</main>
 			</div>
 		)
 	}
