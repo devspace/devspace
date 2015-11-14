3915f74e10d30553f93790b28ccd1e0fc0ee6030
Zeno Rocha <hi@zenorocha.com>
2015-11-13 19:41:57 -0800

    Refactoring Nav styling and markup

--- scripts/nav.js
+++ scripts/nav.js
@@ -3,21 +3,51 @@ import React from 'react';
 class Nav extends React.Component {
 	render() {
 		return (
-			<div className="nav">
-				<ul>
-					<li><a className="nav-selected" href="/" title="Home"><span className="octicon octicon-home"></span></a></li>
-					<li><a href="#" title="Add column"><span className="octicon octicon-plus"></span></a></li>
-					<li><a href="#" title="Settings"><span className="octicon octicon-gear"></span></a></li>
-				</ul>
-				<div className="footer">
-					<ul>
-						<li><a href="#" title="Announcements"><span className="octicon octicon-megaphone"></span></a></li>
-						<li><a href="mailto:support@devspace.io" title="Ask a question"><span className="octicon octicon-question"></span></a></li>
-						<li><a href="https://github.com/devspace/bugs" title="Report a bug"><span className="octicon octicon-bug"></span></a></li>
-						<li><a href="#" title="Sign Out"><span className="octicon octicon-sign-out"></span></a></li>
+			<nav className="nav">
+				<header className="nav-top">
+					<ul className="nav-list">
+						<li className="nav-item">
+							<a className="nav-link nav-selected" href="/" title="Home">
+								<span className="nav-icon octicon octicon-home"></span>
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#" title="Add column">
+								<span className="nav-icon octicon octicon-plus"></span>
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#" title="Settings">
+								<span className="nav-icon octicon octicon-gear"></span>
+							</a>
+						</li>
 					</ul>
-				</div>
-			</div>
+				</header>
+				<footer className="nav-footer">
+					<ul className="nav-list">
+						<li className="nav-item">
+							<a className="nav-link" href="#" title="Announcements">
+								<span className="nav-icon octicon octicon-megaphone"></span>
+							</a>
+						</li>
+						<li className="nav-item">
+							<a href="mailto:support@devspace.io" title="Ask a question">
+								<span className="nav-icon octicon octicon-question"></span>
+							</a>
+						</li>
+						<li className="nav-item">
+							<a href="https://github.com/devspace/bugs" title="Report a bug">
+								<span className="nav-icon octicon octicon-bug"></span>
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#" title="Sign Out">
+								<span className="nav-icon octicon octicon-sign-out"></span>
+							</a>
+						</li>
+					</ul>
+				</footer>
+			</nav>
 		)
 	}
 }
