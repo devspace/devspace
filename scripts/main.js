17e54f58e34e344fed118e029af73077588cd1e8
Zeno Rocha <hi@zenorocha.com>
2015-11-13 19:24:08 -0800

    Removes react-router

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Link } from 'react-router';
 
 import App from './app';
 import Home from './home';
-import NotFound from './notFound';
 
-var routes = (
-	<Router>
-		<Route path="/" component={Home}/>
-		<Route path="/app" component={App}/>
-		<Route path="*" component={NotFound}/>
-	</Router>
-)
-
-ReactDOM.render(routes, document.querySelector('#wrapper'));
+ReactDOM.render(<Home/>, document.querySelector('#wrapper'));
