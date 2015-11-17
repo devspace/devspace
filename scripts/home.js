59171404fe4aaa683bf6a3faebea67f294c4f8bf
Zeno Rocha <hi@zenorocha.com>
2015-11-17 01:34:33 -0600

    Adds react router back again

--- scripts/home.js
+++ scripts/home.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { History } from 'react-router';
 
 class Home extends React.Component {
+	mixins: [History]
+
+	authenticate() {
+		this.history.pushState(null, '/app');
+	}
+
 	render() {
 		return (
 			<div className="home">
@@ -8,7 +15,7 @@ class Home extends React.Component {
 					<img className="home-logo" src="./images/icon.png" height="256" width="256" alt="DevSpace Logo" />
 					<h1 className="home-title">DevSpace</h1>
 					<h2 className="home-subtitle">The best way to stay in touch with what's happening now on GitHub</h2>
-					<button className="home-btn">
+					<button className="home-btn" onClick={this.authenticate}>
 						<span className="octicon octicon-mark-github"></span> Login with GitHub
 					</button>
 				</main>
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route } from 'react-router';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
 
 import App from './app';
 import Home from './home';
+import NotFound from './notFound';
 
-ReactDOM.render(<Home/>, document.querySelector('#wrapper'));
+var routes = (
+	<Router history={createBrowserHistory()}>
+		<Route path="/" component={Home}/>
+		<Route path="/app" component={App}/>
+		<Route path="*" component={NotFound}/>
+	</Router>
+)
+
+ReactDOM.render(routes, document.querySelector('#wrapper'));
