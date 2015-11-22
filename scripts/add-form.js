2255bf70175537a71282d96037c245ccdbebc274
Zeno Rocha <hi@zenorocha.com>
2015-11-22 01:17:35 -0800

    Removes loading state on adding form

--- scripts/add-form.js
+++ scripts/add-form.js
@@ -3,28 +3,12 @@ import React from 'react';
 import { ModalBody, ModalFooter, ModalHeader, FormField, FormInput, FormIconField, Button } from 'elemental/lib/Elemental';
 
 class AddForm extends React.Component {
-	constructor() {
-		super();
-
-		this.state = {
-			loading: false
-		};
-	}
-
 	handleSubmit(event) {
 		event.preventDefault();
 
-		this.setState({
-			loading: true
 		});
 
-		setTimeout(() => {
-			this.setState({
-				loading: false
-			});
-
-			this.props.toggleAddModal();
-		}, 1000);
+		this.props.toggleAddModal();
 	}
 
 	render() {
@@ -39,8 +23,8 @@ class AddForm extends React.Component {
 				</ModalHeader>
 				<ModalBody>
 					<FormField label={"Type a " + this.props.selectedOption.form.label} htmlFor="input-repo">
-						<FormIconField iconPosition="right" iconKey="search" iconColor="default" iconIsLoading={this.state.loading}>
 							<FormInput type="text" placeholder={this.props.selectedOption.form.placeholder} name="input-repo" autoFocus />
+						<FormIconField iconPosition="right" iconKey="search" iconColor="default">
 						</FormIconField>
 					</FormField>
 				</ModalBody>
