256bc97baccf1c91088d50b73d18bc90b9ca7f0d
Zeno Rocha <hi@zenorocha.com>
2016-02-04 12:06:03 -0800

    Adds missing menu options for desktop apps

--- menu.js
+++ menu.js
@@ -1,25 +1,6 @@
 var app = require('app');
 
-module.exports = [
-	{
-		label: 'Application',
-		submenu: [
-			{
-				label: 'About DevSpace',
-				selector: 'orderFrontStandardAboutPanel:'
-			},
-			{
-				type: 'separator'
-			},
-			{
-				label: 'Quit',
-				accelerator: 'Command+Q',
-				click: function() {
-					app.quit();
-				}
-			}
-		]
-	},
+var template = [
 	{
 		label: 'Edit',
 		submenu: [
@@ -56,6 +37,107 @@ module.exports = [
 				accelerator: 'CmdOrCtrl+A',
 				selector: 'selectAll:'
 			}
-  		]
-	}
+		]
+	},
+	{
+		label: 'View',
+		submenu: [
+			{
+				label: 'Reload',
+				accelerator: 'CmdOrCtrl+R',
+				click: function(item, focusedWindow) {
+					if (focusedWindow)
+						focusedWindow.reload();
+				}
+			},
+			{
+				label: 'Toggle Full Screen',
+				accelerator: (function() {
+					if (process.platform == 'darwin')
+						return 'Ctrl+Command+F';
+					else
+						return 'F11';
+				})(),
+				click: function(item, focusedWindow) {
+					if (focusedWindow)
+						focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+				}
+			}
+		]
+	},
+	{
+		label: 'Window',
+		role: 'window',
+		submenu: [
+			{
+				label: 'Minimize',
+				accelerator: 'CmdOrCtrl+M',
+				role: 'minimize'
+			},
+			{
+				label: 'Close',
+				accelerator: 'CmdOrCtrl+W',
+				role: 'close'
+			}
+		]
+	},
+	{
+		label: 'Help',
+		role: 'help',
+		submenu: [
+			{
+				label: 'Learn More',
+				click: function() { require('electron').shell.openExternal('https://devspace.io') }
+			}
+		]
+  }
 ];
+
+if (process.platform == 'darwin') {
+	template.unshift({
+		label: 'DevSpace',
+		submenu: [
+			{
+				label: 'About DevSpace',
+				role: 'about'
+			},
+			{
+				type: 'separator'
+			},
+			{
+				label: 'Hide DevSpace',
+				accelerator: 'Command+H',
+				role: 'hide'
+			},
+			{
+				label: 'Hide Others',
+				accelerator: 'Command+Alt+H',
+				role: 'hideothers'
+			},
+			{
+				label: 'Show All',
+				role: 'unhide'
+			},
+			{
+				type: 'separator'
+			},
+			{
+				label: 'Quit',
+				accelerator: 'Command+Q',
+				click: function() { app.quit(); }
+			}
+		]
+	});
+
+	template[3].submenu.push(
+		{
+			type: 'separator'
+		},
+		{
+			label: 'Bring All to Front',
+			role: 'front'
+		}
+	);
+}
+
+module.exports = template;
